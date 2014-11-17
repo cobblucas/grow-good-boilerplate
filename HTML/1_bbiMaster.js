60cd8d102b797c1df11cde827ea1758f616b0d5f
lucas <lucas.cobb@blackbaud.com>
2014-11-17 16:21:49 -0500

    Committing Grow Layout files

---
+++ HTML/1_bbiMaster.js
@@ -0,0 +1,7 @@
+<!-- BBI NAMESPACE -->
+<script>
+(function(a,c,d){if(!a.getElementById(c)){
+var b=a.createElement("script");b.src=d;b.id=c;
+a.getElementsByTagName("head")[0].appendChild(b)
+}})(document,"bbi-namespace","//api.blackbaud.com/bbi");
+</script>
---
+++ HTML/3_bootstrap.js
@@ -0,0 +1,10 @@
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="//dl.dropboxusercontent.com/u/2966441/repos/pauahi%20foundation/js/ie10-viewport-bug-workaround.js"></script>
+
+<div class="bbi-script" data-bbi-src="//dl.dropboxusercontent.com/u/2966441/repos/pauahi%20foundation/js/bbi.PauahiFoundation.js"></div>
